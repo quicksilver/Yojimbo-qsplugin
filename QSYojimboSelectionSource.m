602e7e9b64d7db8de5908498e41b98347827fe78
Rob McBroom <github@skurfer.com>
2014-01-14 16:19:52 -0500

    don't resolve the selection unless Yojimbo is running

--- QSYojimboSelectionSource.m
+++ QSYojimboSelectionSource.m
@@ -15,19 +15,21 @@
 - (id)resolveProxyObject:(id)proxy
 {
     YojimboApplication *Yojimbo = [SBApplication applicationWithBundleIdentifier:@"com.barebones.yojimbo4"];
-    NSArray *ysel = [Yojimbo selection];
-    if (ysel) {
-        NSMutableArray *selectionObjects = [NSMutableArray array];
-        QSObject *object = nil;
-        for (YojimboDatabaseItem *item in ysel) {
-            // get ID and use that to pull from catalog
-            object = [QSObject objectWithIdentifier:[item id]];
-            if (object) {
-                [selectionObjects addObject:object];
+    if ([Yojimbo isRunning]) {
+        NSArray *ysel = [Yojimbo selection];
+        if (ysel) {
+            NSMutableArray *selectionObjects = [NSMutableArray array];
+            QSObject *object = nil;
+            for (YojimboDatabaseItem *item in ysel) {
+                // get ID and use that to pull from catalog
+                object = [QSObject objectWithIdentifier:[item id]];
+                if (object) {
+                    [selectionObjects addObject:object];
+                }
             }
-        }
-        if ([selectionObjects count]) {
-            return [QSObject objectByMergingObjects:selectionObjects];
+            if ([selectionObjects count]) {
+                return [QSObject objectByMergingObjects:selectionObjects];
+            }
         }
     }
     return nil;
